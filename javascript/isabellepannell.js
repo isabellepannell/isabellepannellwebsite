cbb70d47a3576f4662218225dff1076b98ea16cc
isabellepannell <isabelle.pannell@education.nsw.gov.au>
2021-09-23 23:19:25 +1000

    first commit

---
+++ javascript/isabellepannell.js
@@ -0,0 +1,11 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 0) {
+  document.getElementById("header").style.fontSize = "40px";
+} else {
+  document.getElementById("header").style.fontSize = "60px";
+}
+}
+
